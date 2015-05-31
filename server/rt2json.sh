7879da8cf6c767fbf19a5549bea5914241c27889
Luciano Restifo <luciano.restifo@gmail.com>
2015-05-31 09:05:55 +0000

    Fixed date handling for JavaScript

--- server/rt2json.sh
+++ server/rt2json.sh
@@ -5,11 +5,16 @@
 # Author: Mon May 25 13:48:54 CEST 2015 Luciano Restifo <lrestifo@esselte.com>
 # Description:
 #   This simple script runs a known query against BestPractical's RT and
-#   converts its output to a JSON object array.  Nothing particularly smart
+#   converts its output to a JSON object array.  Nothing particularly smart.
+#   A bit of complication comes from handling of dates, which RT spits out
+#   like "Fri Jan 04 22:06:35 CEST 2015", and jq would preserve as a regular
+#   text string, however they need to be expressed as new Date() in JavaScript.
+#   To handle this I added a bit of awk in the pipeline.
 # Dependencies:
 #   rt      command-line binary does the heavy lifting
 #   .rtrc   contains server configuration (URL and credentials)
 #   jq      parse rt's output to JSON (http://stedolan.github.io/jq/)
+#   awk     helps converting dates to JavaScript expectations
 # Credits:
 #   I took inspiration from this article:
 #     http://infiniteundo.com/post/99336704013/convert-csv-to-json-with-jq
@@ -21,21 +26,21 @@ queues="IT_Germany,IT_Germany_Uelzen"
 fields="Queue,Owner,Subject,Status,Priority,Requestors,Created,Resolved,CF.{Country},CF.{Ticket Classification},CF.{Request_Type}"
 tktsql="Created > '01/01/2015' AND 'CF.{Ticket Classification}' NOT LIKE 'ERP_SAP_%'"
 
-# rt ls -q "$queues" -f "$fields" "$tktsql" | \
-#   jq --slurp --raw-input --raw-output \
-#     'split("\n") | .[1:] | map(split("\t")) |
-#       map({"id": .[0] | tonumber,
-#             "queue": .[1],
-#             "owner": .[2],
-#             "subject": .[3],
-#             "status": .[4],
-#             "priority": .[5],
-#             "requestors": .[6],
-#             "created": .[7],
-#             "resolved": .[8],
-#             "country": .[9],
-#             "requestType": .[10],
-#             "classification": .[11]})' | \
+rt ls -q "$queues" -f "$fields" "$tktsql" | \
+  jq --slurp --raw-input --raw-output \
+    'split("\n") | .[1:] | map(split("\t")) |
+      map({"id": .[0] | tonumber,
+            "queue": .[1],
+            "owner": .[2],
+            "subject": .[3],
+            "status": .[4],
+            "priority": .[5],
+            "requestors": .[6],
+            "created": .[7],
+            "resolved": .[8],
+            "country": .[9],
+            "requestType": .[10],
+            "classification": .[11]})' | \
     awk '
       BEGIN {
         month["Jan"] = 0; month["Feb"] = 1; month["Mar"] = 2; month["Apr"] = 3;
